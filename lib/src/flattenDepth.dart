ad276bad03e3c4227e676bcddd716f2dccb364b2
huanghaiyang <haiyang.huang@forceclouds.com>
2019-06-12 22:43:16 +0800

    update

--- lib/src/flattenDepth.dart
+++ lib/src/flattenDepth.dart
@@ -1,6 +1,6 @@
 import 'package:todash/src/internal/baseFlatten.dart';
 
-List flattenDeep(Object array, [int depth]) {
+List flattenDepth(Object array, [int depth]) {
   if(depth == null) {
     depth = 1;
   }
--- lib/todash.dart
+++ lib/todash.dart
@@ -5,5 +5,7 @@ library todash;
 
 export 'src/get.dart';
 export 'src/flatten.dart';
+export 'src/flattenDeep.dart';
+export 'src/flattenDepth.dart';
 
 // TODO: Export any libraries intended for clients of this package.
